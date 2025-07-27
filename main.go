e092b3efd50dbc469aee3ad10bed3584a626a44a
AbdelghaniDjedidi2001 <abdelghani.djedidi.2022@gmail.com>
2025-07-27 09:34:45 +0100

    try var and const

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
- 
+
 import "fmt"
 
 func main() {
-	fmt.Println("hello word")
+	// var name = "Go Confrerence"
+	const name = "Go Confrerence"
+	const confrerenceTickets = 50
+	var remainingTickets = 50
+	fmt.Println("Welcome to our ", name, "booking application")
+	fmt.Println("we have total of", confrerenceTickets, "tickets and ", remainingTickets, "are still available.")
+	fmt.Println("Get your tickets here to attend")
 }
