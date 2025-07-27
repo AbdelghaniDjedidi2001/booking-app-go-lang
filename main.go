8e534e34e4f1df2dd46556c3dfd73f88c7c2395d
AbdelghaniDjedidi2001 <abdelghani.djedidi.2022@gmail.com>
2025-07-27 10:00:33 +0100

    try data types in go

--- main.go
+++ main.go
@@ -4,10 +4,26 @@ import "fmt"
 
 func main() {
 	// var name = "Go Confrerence"
-	const name = "Go Confrerence"
+	const confrerenceName = "Go Confrerence"
 	const confrerenceTickets = 50
-	var remainingTickets = 50
-	fmt.Println("Welcome to our ", name, "booking application")
-	fmt.Println("we have total of", confrerenceTickets, "tickets and ", remainingTickets, "are still available.")
+	remainingTickets := 50 //same as  var remainingTickets = 50 and work only in var not const 
+
+	fmt.Printf("confrerenceName is %T, confrerenceTickets is %T, remainingTickets is %T\n",confrerenceName,confrerenceTickets,remainingTickets)
+
+	// fmt.Println("Welcome to our ", name, "booking application")
+	// fmt.Println("we have total of", confrerenceTickets, "tickets and ", remainingTickets, "are still available.")
+
+	fmt.Printf("Welcome to our %s booking application\n",confrerenceName)
+	fmt.Printf("we have total of %v tickets and %v are still available.\n",confrerenceTickets,remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	// var userName error
+	var userName string
+	var userTickets int
+
+	userName = "Ghonou"
+	userTickets = 2
+
+	fmt.Printf("User %s booked %v tickets\n",userName,userTickets)
+	
 }
